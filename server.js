e631c9d6e07dbb023a00257855c0035aea583227
Ben <byeungis@gmail.com>
2022-06-28 04:55:44 -0400

    updates

--- server.js
+++ server.js
@@ -37,6 +37,11 @@ const db = mysql.createConnection(
   console.log("Connected to the election database.")
 );
 
+//return all the data in the candidates table, query() method runs the SQL query and executes the callback with all the results rows that match the query.  call back function captures the response, if no err(error) and rows, which is the database query response.  if no errors in sql then err value is null.
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
 //function that will start the Express.js server on port 3001, always at the bottom
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
